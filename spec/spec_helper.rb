001e7d3d8ca0b55c9e826f3a33b57ab746cfdbbf
KATO Kei <k4t0.kei@gmail.com>
2021-09-25 07:53:17 +0900

    :construction_worker: Add CI config.

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -2,7 +2,15 @@
 
 if ENV['COVERAGE'] == 'true'
   require 'simplecov'
+  require 'simplecov-lcov'
+
   SimpleCov.start do
+    SimpleCov::Formatter::LcovFormatter.config do |c|
+      c.report_with_single_file = true
+      c.single_report_path = 'coverage/lcov.info'
+    end
+    formatter SimpleCov::Formatter::LcovFormatter
+
     add_filter '/spec/'
   end
 end
